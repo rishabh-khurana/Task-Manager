598f0f892aa4765a9683c41a5d8d9a9ad913f4c2
Rishabh Khurana <rishabhk994@gmail.com>
2019-12-06 03:05:35 +1100

    navbar and material ui

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css';
+import PrimarySearchAppBar from './NavBar';
+
+function HomePage(){
+    return(
+        <div>
+            <PrimarySearchAppBar/>
+        </div>
+    );
+}
+
+export default HomePage;
