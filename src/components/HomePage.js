f0235a03585e09586942419ca8090e77d80814c0
Rishabh Khurana <rishabhk994@gmail.com>
2019-12-06 16:59:35 +1100

    sections created

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import './HomePage.css';
 import PrimarySearchAppBar from './NavBar';
+import Grid from '@material-ui/core/Grid';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import BuildIcon from '@material-ui/icons/Build';
+import Typography from '@material-ui/core/Typography';
 
 function HomePage(){
     return(
         <div>
             <PrimarySearchAppBar/>
+            <Grid container spacing={1}>
+                <Grid item sm xs={12}>
+                    <div className='list-title'>
+                        <Typography variant='h4'><FileCopyIcon/>To do</Typography>
+                    </div>
+                    <div className='list-container'>Content</div>
+                </Grid>
+                <Grid item sm xs={12}>
+                    <div className='list-title'>
+                        <Typography variant='h4'><BuildIcon/>In Progress</Typography>
+                    </div>
+                    <div className='list-container'>Content</div>
+                </Grid>
+                <Grid item sm xs={12}>
+                    <div className='list-title'>
+                        <Typography variant='h4'><CheckCircleIcon/>Completed</Typography>
+                    </div>
+                    <div className='list-container'>Content</div>
+                </Grid>
+            </Grid>
         </div>
     );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,7 +14,6 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
-import blue from '@material-ui/core/colors/blue';
 
 const useStyles = makeStyles(theme => ({
   grow: {
