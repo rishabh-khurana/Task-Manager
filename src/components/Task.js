754c4b3e1e0a3a7c2589f4121235c89281ad585c
Rishabh Khurana <rishabhk994@gmail.com>
2019-12-08 23:19:15 +1100

    basic card design

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,52 +5,35 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import TimerIcon from '@material-ui/icons/Timer';
 
 const useStyles = makeStyles({
   card: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
   },
   title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
+    fontSize: 20,
+  }
 });
 
 export default function SimpleCard() {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
     <Card className={classes.card}>
       <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
+        <Typography className={classes.title} gutterBottom>
+          Title of the Task
         </Typography>
-        <Typography variant="h5" component="h2">
-          be
-          {bull}
-          nev
-          {bull}o{bull}
-          lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+        <Typography component="p" color="textSecondary">
+          This is the description of the task
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Button><PlayCircleOutlineIcon/>Start</Button>
+        <Button>
+          <TimerIcon/>1h 2m 3s
+        </Button>
       </CardActions>
     </Card>
   );
