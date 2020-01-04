e0d31349769ed1f24e5afa39a4bc8940a246faa8
Rishabh Khurana <rishabhk994@gmail.com>
2020-01-04 22:53:43 +1100

    New Task Dialogue implemented

--- src/components/NewTaskDialog.js
+++ src/components/NewTaskDialog.js
@@ -6,6 +6,18 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import FormControl from '@material-ui/core/FormControl';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import Grid from '@material-ui/core/Grid';
+import 'date-fns';
 
 export default function NewTaskDialog(props) {
 //   const [open, setOpen] = React.useState(false);
@@ -15,8 +27,36 @@ export default function NewTaskDialog(props) {
 //   };
 
 //   const handleClose = () => {
-//     setOpen(false);
+//     setOpen(false);event.target.value
 //   };
+
+  const [task, setTask] = React.useState('');
+  const [desc, setDesc] = React.useState('');
+  const [value, setValue] = React.useState('HIGH');
+
+  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T18:30:00'));
+
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
+
+  const handleDateChange = date => {
+    setSelectedDate(date);
+  };
+
+  const handleSetDescription = (event) => {
+    setDesc(event.target.value);
+  };
+
+  const handleSetTask = (event) => {
+    setTask(event.target.value);
+  };
+
+  const taskCreate = () => {
+    // props.handleClose() implement handle close 
+    
+    // send all values to parent 
+  }
 
   return (
     <div>
@@ -29,25 +69,61 @@ export default function NewTaskDialog(props) {
           <TextField
             autoFocus
             margin="dense"
-            id="name"
+            id="task-name"
             label="Task Name"
             type="email"
             fullWidth
+            onBlur={handleSetTask}
           />
           <TextField
             autoFocus
             margin="dense"
-            id="name"
+            id="desc"
             label="Description"
             type="email"
             fullWidth
+            onBlur={handleSetDescription}
           />
+          <DialogContentText style={{paddingTop:"2rem"}}>
+            Task Importance Level (High/Low)
+          </DialogContentText>
+        <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+          <FormControlLabel value="HIGH" control={<Radio />} label="High" />
+          <FormControlLabel value="LOW" control={<Radio />} label="Low" />
+        </RadioGroup>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <Grid container justify="space-around">
+            <KeyboardDatePicker
+            disableToolbar
+            variant="inline"
+            format="MM/dd/yyyy"
+            margin="normal"
+            id="date-picker-inline"
+            label="Task End Date"
+            value={selectedDate}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+            />
+            <KeyboardTimePicker
+            margin="normal"
+            id="time-picker"
+            label="Task End Time"
+            value={selectedDate}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change time',
+            }}
+            />
+          </Grid>
+        </MuiPickersUtilsProvider>
         </DialogContent>
         <DialogActions>
           <Button onClick={props.dialogCloseHandler} color="primary">
             Cancel
           </Button>
-          <Button onClick={props.handleClose} color="primary">
+          <Button onClick={taskCreate} color="primary">
             Create
           </Button>
         </DialogActions>
